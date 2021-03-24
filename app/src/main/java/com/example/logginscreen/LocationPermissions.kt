5c9364ccf780cea007a8b02169de3631c0e67b94
Jose-Luna8480 <jose.luna8480@gmail.com>
2021-03-23 22:00:50 -0600

    First commit: First login screen and intent to second screen

---
+++ app/src/main/java/com/example/logginscreen/LocationPermissions.kt
@@ -0,0 +1,11 @@
+package com.example.logginscreen
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LocationPermissions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location_permissions)
+    }
+}
---
+++ app/src/main/java/com/example/logginscreen/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.logginscreen
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val sharedPreferences = getSharedPreferences("SP_INFO", Context.MODE_PRIVATE)
+        val username = sharedPreferences.getString("UserName", "")
+        val password = sharedPreferences.getString("Password", "")
+        if (username != null && password != null) {
+            val intent = Intent(this, LocationPermissions::class.java).apply {}
+            startActivity(intent)
+        }
+
+        val logInButton: Button = findViewById(R.id.button)
+        logInButton.setOnClickListener { obtainUserAndPassword() }
+    }
+
+    fun obtainUserAndPassword(){
+        val sharedPreferences = getSharedPreferences("SP_INFO", Context.MODE_PRIVATE)
+
+        val userEditText = findViewById<View>(R.id.Username) as EditText
+        val userText = userEditText.text.toString()
+
+        val passwordEditText = findViewById<View>(R.id.Password) as EditText
+        val passwordText = passwordEditText.text.toString()
+
+        if (userText.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(userText).matches()){
+            Toast.makeText(this.applicationContext, "Invalid user or password", Toast.LENGTH_SHORT).show()
+        }else{
+            //Toast.makeText(this.applicationContext, "The user is: ${sharedPreferences.getString("UserName", "")} and the password is: ${sharedPreferences.getString("Password", "")}", Toast.LENGTH_SHORT).show()
+            val editor = sharedPreferences.edit()
+            editor.putString("UserName", userText)
+            editor.putString("Password", passwordText)
+            editor.apply()
+            val intent = Intent(this, LocationPermissions::class.java).apply {}
+            startActivity(intent)
+        }
+    }
+}
